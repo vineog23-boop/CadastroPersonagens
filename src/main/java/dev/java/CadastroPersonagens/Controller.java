d4692e2572cdfa3dfb32c33557a6a675ddf82d56
vineog23-boop <vine.og23@gmail.com>
2026-03-03 18:50:48 -0300

    Controller Atualizado

--- src/main/java/dev/java/CadastroPersonagens/Controller.java
+++ src/main/java/dev/java/CadastroPersonagens/Controller.java
@@ -1,4 +1,18 @@
 package dev.java.CadastroPersonagens;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas (){
+        return "Essa é minha primeira mensagem nessa rota";
+
+    }
+
 }
