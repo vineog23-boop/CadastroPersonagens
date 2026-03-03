05fcb7b14c7de160195355e56ea04235a70b7df6
vineog23-boop <vine.og23@gmail.com>
2026-03-03 17:18:01 -0300

    first commit

---
+++ src/main/java/dev/java/CadastroPersonagens/CadastroPersonagensApplication.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroPersonagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroPersonagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroPersonagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java/CadastroPersonagens/CadastroPersonagensApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroPersonagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPersonagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
