9fc4a5f5db960b33f44ca702b6d027f280625686
vineog23-boop <vine.og23@gmail.com>
2026-03-07 19:03:40 -0300

    feat: variáveis de ambiente, reorganiza estrutura e atualiza repositório

--- src/main/java/dev/java/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/java/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -1,4 +1,4 @@
-package dev.java.CadastroDeNinjas.Ninjas;
+package dev.java.CadastroDeNinjas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,6 @@
 package dev.java.CadastroDeNinjas.Ninjas;
 
-public class NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
 }
